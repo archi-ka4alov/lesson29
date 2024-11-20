0cf6526ea1da6c1436b9e98900177257c4849591
archi-ka4alov <archi.ka4alov@gmail.com>
2024-11-20 22:38:14 +0300

    Add files via upload

---
+++ module_7_4.py
@@ -0,0 +1,37 @@
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+time_avg = 45.2
+
+# количество участников первой команды:
+print('В команде Мастера кода участников: %s !' % team1_num)
+
+# количество участников в обеих командах:
+print('Итого сегодня в командах участников: %s и %s !' % (team1_num, team2_num))
+
+# количество задач решённых командой 2:
+print('Команда Волшебники данных решила задач: {} !'.format(score_2))
+
+# время за которое команда 2 решила задачи:
+print('Волшебники данных решили задачи за {0:.1f} с !'.format(team2_time))
+
+# количество решённых задач по командам:
+print(f'Команды решили {score_1} и {score_2} задач.')
+
+# исход соревнования:
+if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
+    result = 'Победа команды Мастера кода!'
+elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
+    result = 'Победа команды Волшебники Данных!'
+else:
+    result = 'Ничья!'
+
+print(f'Результат битвы: {result}')
+
+# количество задач и среднее время решения:
+tasks_total = score_1 + score_2
+time_avg = (team1_time + team2_time) / tasks_total
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {round(time_avg, 1)} секунды на задачу!')
